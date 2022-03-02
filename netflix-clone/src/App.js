22ce06f716a9126855aa7d0523ece2f1ce495e50
Hoon-Hub <gwi01304@naver.com>
2022-03-02 17:13:06 +0900

    get movie list & style

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>this is react start</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,44 @@
+// component
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original";
+
+const Row = ({ title, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
+
+  // Rows 컴포넌트가 rendering 될 때 해당 내용이 출력됨
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]); // if [], -> 1번만 실행.
+  // fetchUrl을 넣었으므로, 새로고침마다 정보를 업데이트해준다.
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {/* row__poster */}
+
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={`${base_url}${movie.name}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
