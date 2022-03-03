7da3ce3cf3011d34af9b1a384d6ba9a5c8440b04
Hoon-Hub <gwi01304@naver.com>
2022-03-03 18:29:26 +0900

    banner

---
+++ airbnb-clone/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Home from "./Home.js";
+import Header from "./Header.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+
+      {/* Home */}
+      {/* Header */}
+      {/* Banner > Search*/}
+      {/* content */}
+
+      {/* SearchPage */}
+      {/* footer */}
+    </div>
+  );
+}
+
+export default App;
---
+++ airbnb-clone/src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Banner from "./Banner.js";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Banner />
+      <div className="home__section"></div>
+    </div>
+  );
+};
+
+export default Home;
