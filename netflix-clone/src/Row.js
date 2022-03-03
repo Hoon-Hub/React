594d0a96afc8b08cad72ab58a1312197a9930746
Hoon-Hub <gwi01304@naver.com>
2022-03-03 13:02:39 +0900

    firebase uploaded

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -3,11 +3,14 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // Rows 컴포넌트가 rendering 될 때 해당 내용이 출력됨
   useEffect(() => {
@@ -21,16 +24,35 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
   }, [fetchUrl]); // if [], -> 1번만 실행.
   // fetchUrl을 넣었으므로, 새로고침마다 정보를 업데이트해준다.
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((err) => console.log(err));
+    }
+  };
   return (
     <div className="row">
       <h2>{title}</h2>
 
       <div className="row__posters">
-        {/* row__poster */}
-
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={handleClick}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -39,6 +61,7 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId="{trailerUrl}" opts={opts} />}
     </div>
   );
 };
