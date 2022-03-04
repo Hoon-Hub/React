002bf106bcbacc2f0dfe0eddf4c931afff5df534
Hoon-Hub <gwi01304@naver.com>
2022-03-04 11:09:30 +0900

    ui end

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Home from "./Home.js";
 import Header from "./Header.js";
+import Footer from "./Footer.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
-
+      <Footer />
       {/* Home */}
       {/* Header */}
       {/* Banner > Search*/}
---
+++ airbnb-clone/src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>ⓒ2022 Airbnb</p>
+      <p>Copy right to me</p>
+    </div>
+  );
+};
+
+export default Footer;
