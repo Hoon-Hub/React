8dd2dd148d008384a74e8fe14b3dbb22e35fdc3f
Hoon-Hub <gwi01304@naver.com>
2022-03-04 14:38:30 +0900

    navigate

--- airbnb-clone/src/Search.js
+++ airbnb-clone/src/Search.js
@@ -6,8 +6,10 @@ import "react-date-range/dist/theme/default.css";
 import { useState } from "react";
 import PeopleIcon from "@material-ui/icons/People";
 import { Button } from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
 // DatePicker component
 const Search = () => {
+  const navigate = useNavigate();
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
@@ -30,7 +32,7 @@ const Search = () => {
         <PeopleIcon />
       </h2>
       <input min={0} defaultValue={2} type="number" />
-      <Button>Search Airbnb</Button>
+      <Button onClick={() => navigate("/search")}>Search Airbnb</Button>
     </div>
   );
 };
