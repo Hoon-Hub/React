8c14b11d71c0de24f063713803f09f52d5075beb
Hoon-Hub <gwi01304@naver.com>
2022-03-04 14:06:15 +0900

    search area

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -2,20 +2,21 @@ import "./App.css";
 import Home from "./Home.js";
 import Header from "./Header.js";
 import Footer from "./Footer.js";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SearchPage from "./SearchPage.js";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-      {/* Home */}
-      {/* Header */}
-      {/* Banner > Search*/}
-      {/* content */}
+      <Router>
+        <Header />
 
-      {/* SearchPage */}
-      {/* footer */}
+        <Routes>
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
