d263190735436e5ad9ecdea0f90eb605358a00ad
Hoon-Hub <gwi01304@naver.com>
2022-03-10 17:18:50 +0900

    navigate

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -4,6 +4,7 @@ import About from "./About";
 import "./App.css";
 import Home from "./Home";
 import Profiles from "./Profiles";
+import Navigate from "./Navigate.js";
 function App() {
   return (
     <div className="App">
@@ -17,11 +18,15 @@ function App() {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/navigate">Navigate</Link>
+        </li>
       </ul>
       <Routes>
         <Route path="/" element={<Home />} exact={true} />
         <Route path="/about" element={<About />} />
         <Route path="/profiles/*" element={<Profiles />} />
+        <Route path="/navigate" element={<Navigate />} />
       </Routes>
     </div>
   );
