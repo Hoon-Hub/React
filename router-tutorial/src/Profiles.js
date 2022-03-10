63f09fd792beff3a6c7ba1a3f09b49547c847548
Hoon-Hub <gwi01304@naver.com>
2022-03-10 17:58:39 +0900

    router end

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Routes, Route } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import Profile from "./Profile";
 
 const Profiles = () => {
@@ -8,10 +8,10 @@ const Profiles = () => {
       <h3>사용자 목록</h3>
       <ul>
         <li>
-          <Link to="profiles/sanghoon">Sanghoon</Link>
+          <NavLink to="profiles/sanghoon">Sanghoon</NavLink>
         </li>
         <li>
-          <Link to="profiles/gildong">Gildong</Link>
+          <NavLink to="profiles/gildong">Gildong</NavLink>
         </li>
       </ul>
       <Routes>
