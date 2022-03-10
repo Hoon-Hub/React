bd340e38f2aa6a1453b087a9327c9adcb2e8d495
Hoon-Hub <gwi01304@naver.com>
2022-03-10 21:25:25 +0900

    NewsItem

--- news-viewer/src/components/NewsItem.js
+++ news-viewer/src/components/NewsItem.js
@@ -38,7 +38,25 @@ const NewsItemBlock = styled.div`
 
 const NewsItem = ({ article }) => {
   const { title, description, url, urlToImage } = article;
-  return <div>NewsItem</div>;
+  return (
+    <NewsItemBlock>
+      {urlToImage && (
+        <div className="thumbnail">
+          <a href={url} target="_blank" rel="nooper noreferrer">
+            <img src={urlToImage} alt="thumbnail" />
+          </a>
+        </div>
+      )}
+      <div className="contents">
+        <h2>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </h2>
+        <p>{description}</p>
+      </div>
+    </NewsItemBlock>
+  );
 };
 
 export default NewsItem;
