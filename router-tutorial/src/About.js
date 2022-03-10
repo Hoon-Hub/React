2d407233e90af31d6b755e89a97446bbfb61f3af
Hoon-Hub <gwi01304@naver.com>
2022-03-10 15:52:56 +0900

    url query string - useSearchParams

--- router-tutorial/src/About.js
+++ router-tutorial/src/About.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
-const About = () => {
+const About = ({ location }) => {
+  console.log(location);
+  const [query] = useSearchParams();
+  const showDetail = query.get("detail") === "true";
+  console.log(showDetail);
+
   return (
     <div>
       <h1>About</h1>
       <p>About page</p>
+      Query String: {showDetail && <p>detail 값을 true로 설정하셨군요</p>}
     </div>
   );
 };
