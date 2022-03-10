7e3a5fbe34c65ab6833ca79228219c4d7c712d85
Hoon-Hub <gwi01304@naver.com>
2022-03-10 11:05:06 +0900

    produce 사용하기

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -1,7 +1,93 @@
-import "./App.css";
+import React, { useCallback, useRef, useState } from "react";
+import produce from "immer";
+const App = () => {
+  const nextId = useRef(1);
+  const [form, setForm] = useState({ name: "", username: "" });
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null,
+  });
 
-function App() {
-  return <div className="App"></div>;
-}
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setForm({
+        ...form,
+        [name]: [value],
+      });
+    },
+    [form]
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username,
+      };
+
+      // arrray 에 새로운 항목 등록
+      setData(
+        produce((draft) => {
+          draft.array.push(info);
+        })
+      );
+
+      // form 초기화
+      setForm({
+        name: "",
+        username: "",
+      });
+      nextId.current += 1;
+    },
+    [data, form.name, form.username]
+  );
+
+  // 항목을 삭제하는 함수
+  const onRemove = useCallback(
+    (id) => {
+      setData({
+        produce((draft) => {
+          draft.array.splice(draft.array.findIndex(info => info.id === id), 1)
+        })
+      );
+    },
+    []
+  );
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="username"
+          placeholder="아이디"
+          value={form.username}
+          onChange={onChange}
+          type="text"
+        />
+        <input
+          type="text"
+          name="name"
+          placeholder="이름"
+          value={form.name}
+          onChange={onChange}
+        />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <h1>하단 리스트</h1>
+        <ul>
+          {data.array.map((info) => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username} ({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default App;
