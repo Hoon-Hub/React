b8771f76cdac6101ce2aaaa360defda4b66d6d99
Hoon-Hub <gwi01304@naver.com>
2022-03-10 22:17:56 +0900

    category list

---
+++ news-viewer/src/components/Categories.js
@@ -0,0 +1,101 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const categories = [
+  {
+    name: "all",
+    text: "전체보기",
+  },
+  {
+    name: "business",
+    text: "비즈니스",
+  },
+  {
+    name: "entertainment",
+    text: "엔터테인먼트",
+  },
+  {
+    name: "health",
+    text: "건강",
+  },
+  {
+    name: "sceince",
+    text: "과학",
+  },
+  {
+    name: "sports",
+    text: "스포츠",
+  },
+  {
+    name: "technology",
+    text: "기술",
+  },
+];
+
+const CategoriesBlock = styled.div`
+  display: flex;
+  padding: 1rem;
+  width: 768px;
+  height: 30px;
+  margin: 0 auto;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    overflow-x: auto;
+  }
+
+  h1 {
+    margin-top: -10px;
+    margin-left: 10px;
+    margin-right: 20px;
+  }
+`;
+
+const Category = styled.div`
+  font-size: 1.125rem;
+  cursor: pointer;
+  white-space: pre;
+  text-decoration: none;
+  color: inherit;
+  padding-bottom: 0.25rem;
+
+  &:hover {
+    color: #495057;
+    font-weight: bold;
+    border-bottom: 3px solid black;
+    transition: 0.2s;
+  }
+
+  ${(props) =>
+    props.active &&
+    css`
+      font-weight: 600;
+      border-bottom: 2px solid #22b8cf;
+      color: #22b8cf;
+      &:hover {
+        color: #3bc9db;
+      }
+    `}
+
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+const Categories = ({ onSelect, category }) => {
+  return (
+    <CategoriesBlock>
+      <h1>News</h1>
+      {categories.map((c) => (
+        <Category
+          key={c.name}
+          active={category === c.name}
+          onClick={() => onSelect(c.name)}
+        >
+          {c.text}
+        </Category>
+      ))}
+    </CategoriesBlock>
+  );
+};
+
+export default Categories;
--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import axios from "../../node_modules/axios/index";
+import axios from "axios";
 import NewsItem from "./NewsItem";
 
 const NewsListBlock = styled.div`
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
   const API_KEY = "40ed449628a3495caee1163ce660ce7e";
@@ -25,8 +25,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true); //loading
       try {
+        const query = category === "all" ? "" : `&category=${category}`;
         const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${API_KEY}`
         );
         setArticles(response.data.articles);
       } catch (e) {
@@ -35,7 +36,7 @@ const NewsList = () => {
       setLoading(false); // loading
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   if (!articles) {
     // axios를 통해 데이터를 못받아왔을 경우 null 로 설정
