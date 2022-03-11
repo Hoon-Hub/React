d4a4f699f24ed64348526b6921877382f3227fd8
Hoon-Hub <gwi01304@naver.com>
2022-03-11 14:34:53 +0900

    context

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import ColorBox from "./components/ColorBox";
+import { ColorProvider } from "./contexts/color";
+
+function App() {
+  return (
+    <ColorProvider>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorProvider>
+  );
+}
+
+export default App;
