9f9f866529e2683fcd5acce02b112dfe003b553c
Hoon-Hub <gwi01304@naver.com>
2022-03-12 18:47:53 +0900

    useDispatch를 사용시, useCallback 과 함께 사용하도록 한다

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,10 +1,22 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Counter from "../components/Counter";
+import { increase, decrease } from "../modules/counter";
 
 const CounterContainer = () => {
   const number = useSelector((state) => state.counter.number);
-  return <Counter number={number} />;
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
+  return (
+    <Counter
+      number={number}
+      // useDispatch 를 사용할 때에는 useCallback과 함께 쓰도록 한다.
+      onIncrease={onIncrease()}
+      onDecrease={onDecrease()}
+    />
+  );
 };
 
 export default CounterContainer;
