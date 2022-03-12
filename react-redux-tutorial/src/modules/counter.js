7f05ff2664a0f62ebab7c27deeec8f8cd2e63900
Hoon-Hub <gwi01304@naver.com>
2022-03-12 18:06:54 +0900

    createAction handleActions - counter number

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,4 +1,5 @@
 import { createAction } from "redux-actions";
+import handleActions from "redux-actions/lib/handleActions";
 
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
@@ -10,19 +11,12 @@ const initialState = {
   number: 0,
 };
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
 
 export default counter;
