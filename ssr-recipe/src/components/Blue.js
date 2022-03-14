306f3d9ae6c304abf9e9fd2a7a2b1a46af070a2b
Hoon-Hub <gwi01304@naver.com>
2022-03-14 16:40:51 +0900

    before eject

---
+++ ssr-recipe/src/components/Blue.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./blue.css";
+const Blue = () => {
+  return <div className="blue">blue</div>;
+};
+
+export default Blue;
---
+++ ssr-recipe/src/components/Red.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./red.css";
+
+function Red() {
+  return <div className="red">red</div>;
+}
+
+export default Red;
