bd627280f012235f84d21ab71c7ee46407134447
Hoon-Hub <gwi01304@naver.com>
2022-03-15 10:02:28 +0900

    koa router diff

--- blog-app/blog-backend/src/index.js
+++ blog-app/blog-backend/src/index.js
@@ -1,5 +1,5 @@
 const Koa = require("koa");
-const Router = require("koa-router");
+const Router = require("koa-tree-router");
 
 const app = new Koa();
 const router = new Router();
@@ -12,7 +12,7 @@ router.get("/about", (ctx) => {
   ctx.body = "소개";
 });
 
-app.use(router.routes()).use(router.allowedMethods());
+app.use(router.routes());
 
 app.listen(4000, () => {
   console.log("Listening to Port 4000...");
