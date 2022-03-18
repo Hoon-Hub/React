0fc07a9194374666d95c750a5428538ba4f5be4f
Hoon-Hub <gwi01304@naver.com>
2022-03-18 18:02:56 +0900

    redux01

---
+++ understand-redux/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { Counter } from "./features/counter/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Redux Trainning</h1>
+      <Counter></Counter>
+    </div>
+  );
+}
+
+export default App;
