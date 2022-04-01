ce4a642704696d2b509aaf79d874c3929c18135b
Hoon-Hub <gwi01304@naver.com>
2022-04-01 16:30:47 +0900

    diary editing tool

---
+++ simple-diary/src/DiaryEditor.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장성공");
+  };
+
+  return (
+    <>
+      <StyledDiv>
+        <h2>오늘의 일기</h2>
+        <div>
+          <input
+            type="text"
+            name="author"
+            value={state.author}
+            onChange={handleChangeState}
+          />
+        </div>
+        <div>
+          <textarea
+            name="content"
+            value={state.content}
+            onChange={handleChangeState}
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+        <div>
+          <span>오늘의 기분은? &nbsp;&nbsp;</span>
+          <select name="emotion" onChange={handleChangeState}>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+        </div>
+        <div>
+          <button type="button" onClick={handleSubmit}>
+            Save
+          </button>
+        </div>
+      </StyledDiv>
+    </>
+  );
+};
+
+export default DiaryEditor;
+
+const StyledDiv = styled.div`
+  border: 1px solid gray;
+  text-align: center;
+  padding: 20px;
+
+  & input,
+  textarea {
+    margin-bottom: 20px;
+    width: 500px;
+    padding: 10px;
+  }
+
+  & textarea {
+    height: 150px;
+  }
+
+  & select {
+    width: 300px;
+    padding: 10px;
+    margin-bottom: 20px;
+  }
+
+  & button {
+    width: 500px;
+    padding: 10px;
+    cursor: pointer;
+    border-radius: 5px 5px 5px;
+    outline: none;
+    background: #fff;
+  }
+`;
