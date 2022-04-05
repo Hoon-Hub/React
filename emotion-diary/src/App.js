f029be1b758785021d8f48770ebf5db07b8fdc02
Hoon-Hub <gwi01304@naver.com>
2022-04-05 14:57:15 +0900

    header

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -5,21 +5,42 @@ import Edit from "./pages/Edit";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Error from "./pages/Error";
-import RouteTest from "./components/RouteTest";
+
+// components
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽버튼"} onClick={() => alert("왼쪽")} />
+          }
+          rightChild={
+            <MyButton text={"오른쪽버튼"} onClick={() => alert("오른쪽")} />
+          }
+        />
         <h2>App.js</h2>
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        ></MyButton>
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        ></MyButton>
+        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")}></MyButton>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/diary" element={<Diary />} />
+          <Route path="/diary/:id" element={<Diary />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/new" element={<New />} />
-          <Route path="*" element={<Error />} />
         </Routes>
-        <RouteTest></RouteTest>
       </div>
     </BrowserRouter>
   );
--- emotion-diary/src/pages/Edit.js
+++ emotion-diary/src/pages/Edit.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
-  return <div>Edit</div>;
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const who = searchParams.get("who");
+  console.log(id);
+  console.log(who);
+
+  return (
+    <div>
+      <h2>Edit</h2>
+      <button onClick={() => setSearchParams({ who: "sanghoon" })}>
+        CLick
+      </button>
+      <button onClick={() => navigate("home")}>nav</button>
+    </div>
+  );
 };
 
 export default Edit;
