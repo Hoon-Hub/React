538a93cdc4b816e94ad86cedd0370c7f3fe47a85
Hoon-Hub <gwi01304@naver.com>
2022-04-05 16:36:46 +0900

    sorting diarylist

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -35,8 +35,53 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+  {
+    id: 1,
+    emotion: 5,
+    content: "오늘의일기1",
+    date: 1649141606379,
+  },
+  {
+    id: 2,
+    emotion: 2,
+    content: "오늘의일기2",
+    date: 1649141606380,
+  },
+  {
+    id: 3,
+    emotion: 4,
+    content: "오늘의일기3",
+    date: 1649141606381,
+  },
+  {
+    id: 4,
+    emotion: 1,
+    content: "오늘의일기4",
+    date: 1649141606382,
+  },
+  {
+    id: 5,
+    emotion: 1,
+    content: "오늘의일기5",
+    date: 1649141606383,
+  },
+  {
+    id: 6,
+    emotion: 1,
+    content: "오늘의일기6",
+    date: 1649141606384,
+  },
+  {
+    id: 7,
+    emotion: 1,
+    content: "오늘의일기7",
+    date: 1749141606384,
+  },
+];
+
 function App() {
-  const [data, dispatch] = useReducer(reducer, []);
+  const [data, dispatch] = useReducer(reducer, dummyData);
 
   const dataId = useRef(0);
   // create
